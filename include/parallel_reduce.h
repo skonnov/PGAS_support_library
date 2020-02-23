e3ca3712700c1bf748f36e4528f2e241a8f0756f
Sergey Konnov <sergeyzorro007@gmail.com>
2020-02-23 23:07:41 +0300

    parallel_reduce bug fix

--- include/parallel_reduce.h
+++ include/parallel_reduce.h
@@ -7,8 +7,11 @@
 #include "parallel_vector.h"
   // std::function<int(int, int)>reduction - ?
   // std::function<int(int, int, const parallel_vector&, int)> func - ?
+
+#define REDUCE_TAG 456
+
 template<class Reduction>
-int reduce_operation(int ans, const Reduction& reduction, int proccess_begin, int proccess_end, int proccess = 0) {
+int reduce_operation(int ans, const Reduction& reduction, int process_begin, int process_end, int process = 0) {
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -16,9 +19,9 @@ int reduce_operation(int ans, const Reduction& reduction, int proccess_begin, in
     int tmprank = 0;
     int t = 1;
     std::vector<int>vtmprank(size+100);
-    vtmprank[0] = proccess;
-    for(int i = proccess_begin; i <= proccess_end; i++) {
-        if(i == proccess)
+    vtmprank[0] = process;
+    for(int i = process_begin; i <= process_end; i++) {
+        if(i == process)
             continue;
         vtmprank[t++] = i;
         if(rank == i)
@@ -34,14 +37,14 @@ int reduce_operation(int ans, const Reduction& reduction, int proccess_begin, in
             int sender = vtmprank[tmprank + n/(2*i)];
             if(tmprank + n/(2*i) >= t)
                 continue;
-            MPI_Recv(&tmp, 1, MPI_INT, sender, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            MPI_Recv(&tmp, 1, MPI_INT, sender, REDUCE_TAG, MPI_COMM_WORLD, &status);
             tmpans = reduction(tmp, tmpans);
         }
         else
         {
-            MPI_Request request;
             int destination = vtmprank[tmprank - n/(2*i)];
-            MPI_Isend(&tmpans, 1, MPI_INT, destination, 0, MPI_COMM_WORLD, &request);
+            tmpans = 0;
+            MPI_Send(&tmpans, 1, MPI_INT, destination, REDUCE_TAG, MPI_COMM_WORLD);
             break;
         }        
     }
@@ -49,24 +52,24 @@ int reduce_operation(int ans, const Reduction& reduction, int proccess_begin, in
 }
 
 template<class Func, class Reduction>
-int parallel_reduce(int l, int r, const parallel_vector& pv, int identity, const Func& func, const Reduction& reduction, int proccess = 0) {
+int parallel_reduce(int l, int r, const parallel_vector& pv, int identity, const Func& func, const Reduction& reduction, int process = 0) {
     int rank, size;
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    int proccess_begin = pv.get_index_of_proccess(l);
-    int proccess_end = pv.get_index_of_proccess(r-1);
+    int process_begin = pv.get_index_of_process(l);
+    int process_end = pv.get_index_of_process(r-1);
     int ans = identity;
-    if(rank >= proccess_begin && rank <= proccess_end) {
+    if(rank >= process_begin && rank <= process_end) {
         int begin = 0, end = pv.get_portion();
-        if(rank == proccess_begin)
+        if(rank == process_begin)
             begin = pv.get_index_of_element(l);
-        if(rank == proccess_end)
+        if(rank == process_end)
             end = pv.get_index_of_element(r-1)+1;
         ans = func(begin, end, identity);
     }
-    else if(rank != proccess)
+    else if(rank != process)
         return ans;
-    return reduce_operation(ans, reduction, proccess_begin, proccess_end, proccess);
+    return reduce_operation(ans, reduction, process_begin, process_end, process);
 }
 
 #endif // __PARALLEL_REDUCE_H__
