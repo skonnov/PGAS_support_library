87baac55695923b0aa8c736fe973d1a88981a041
Sergey Konnov <sergeyzorro007@gmail.com>
2020-04-22 00:25:04 +0300

    some changes in sum_of_vector sample

--- samples/sum_of_vector.cpp
+++ samples/sum_of_vector.cpp
@@ -17,20 +17,22 @@ int main(int argc, char** argv) {
     int rank, size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    for(int i = 0; i < n; i++) {
-        pv.set_elem(i, i);
-    }
-    double t1 = MPI_Wtime();
-    int sum = 0;
-    if(rank == 0) {
+    if(rank != 0) {
         for(int i = 0; i < n; i++) {
-            int tmp = pv.get_elem(i);
-            sum += pv.get_elem(i);
+            pv.set_elem(i, i);
         }
+        double t1 = MPI_Wtime();
+        int sum = 0;
+        if(rank == 1) {
+            for(int i = 0; i < n; i++) {
+                int tmp = pv.get_elem(i);
+                sum += pv.get_elem(i);
+            }
+        }
+        double t2 = MPI_Wtime();
+        if(rank == 1)
+            cout<<t2-t1<<" "<<sum<<"\n";
     }
-    double t2 = MPI_Wtime();
-    if(rank == 0)
-        cout<<t2-t1;
     mm.finalize();
     std::cout<<"end of main\n";
     return 0;
