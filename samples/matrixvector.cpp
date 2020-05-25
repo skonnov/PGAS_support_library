cb199ea066845702b6f0d52e4bed16694fb515ca
Sergey Konnov <sergeyzorro007@gmail.com>
2020-05-26 01:36:13 +0300

    some changes in sample

--- samples/matrixvector.cpp
+++ samples/matrixvector.cpp
@@ -4,34 +4,6 @@
 #include "memory_manager.h"
 #include "parallel_vector.h"
 
-
-// class Func {
-// public: 
-//     parallel_vector* a;
-//     std::vector<int>* b;
-//     Func(parallel_vector& pv, std::vector<int>&v):a(&pv), b(&v) {}
-//     int operator()(int l, int r, int identity)  const {
-//         int ans = identity;
-//         for(int i = l; i < r; i++) {
-//             int rank;
-//             MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-//             ans+=a->get_elem_proc(i)*(*b)[a->get_global_index_of_element(i, rank)%(b->size())];
-//         }
-//         return ans;
-//     }  
-// };
-
-// class Reduction {
-//     int a, b;
-// public:
-//     Reduction(): a(0), b(0) {}
-//     Reduction(int aa, int bb): a(aa), b(bb) {}
-//     int operator()(int a, int b) const {
-//         return a+b;
-//     }
-// };
-
-
 int main(int argc, char** argv) { // b*a
     mm.memory_manager_init(argc, argv);
     double t1 = MPI_Wtime();
@@ -46,16 +18,17 @@ int main(int argc, char** argv) { // b*a
     if (rank != 0) {
         int worker_rank = rank - 1;
         int worker_size = size - 1;
-        int portion = m/worker_size + (worker_rank < m%worker_size?1:0);
-        int index = 0;
+        int portion = m/worker_size + (worker_rank < m%worker_size?1:0);  // количество элементов на отдельном процессе
+        int index = 0;  // глобальный индекс, элементы [index, index+portion*n) хранятся на одном процессе
         if (worker_rank < m%worker_size) {
             index = portion*worker_rank*n;
         } else {
             index = (portion+1)*(m%worker_size)*n + portion*(worker_rank-m%worker_size)*n;
-        }
+        }  
+        // инициализация
         for (int i = index; i < index + portion*n; i++) {
             pv.set_elem(i, i);
-        }
+        }  
         for (int i = 0; i < n; i++)
             b[i] = i;
         mm.change_mode(READ_ONLY);
@@ -68,7 +41,7 @@ int main(int argc, char** argv) { // b*a
             }
             t++;
         }
-        mm.change_mode(READ_WRITE);
+        mm.change_mode(READ_WRITE);  // копирование элементов в вектор, с которого можно будет получить данные на любом процессе
         for (int i = index/n; i < index/n + portion; i++) {
             ans.set_elem(i, tmp_ans[i-index/n]);
         }
