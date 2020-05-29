4a5e4136e4519bc96d9549fc3cea76403241cb2e
Sergey Konnov <sergeyzorro007@gmail.com>
2020-05-29 20:19:29 +0300

    very small update

--- samples/locks_simple_sample.cpp
+++ samples/locks_simple_sample.cpp
@@ -22,7 +22,7 @@ int main(int argc, char** argv) {
         }
     }
     MPI_Barrier(MPI_COMM_WORLD);
-    if (rank != 0) {        
+    if (rank != 0) {
         for(int i = 0; i < n; i++)
         {
             pv.set_lock(pv.get_quantum(i));
