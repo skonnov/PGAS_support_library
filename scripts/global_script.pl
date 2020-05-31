bb889098b831effc93dcde0403c246f4f0c7fbb4
Sergey Konnov <sergeyzorro007@gmail.com>
2020-06-01 02:50:07 +0300

    fix script

--- scripts/global_script.pl
+++ scripts/global_script.pl
@@ -15,15 +15,15 @@ while($it_sov < $it_sov_max || $it_mv < $it_mv_max) {
                 $tmp = `mpiexec -n $j ../build/Release/parallel_reduce_sum_of_vector $it_sov`;
                 $result = min($result + 0.0, $tmp+0.0);
             }
-            print WF2 $result;
-            print WF2 " ";
+            print WF $result;
+            print WF " ";
         }
         print "parallel_reduce for $it_sov\n";
         $it_sov += $it_sov_step;
         close(WF);
     }
     if ($it_mv < $it_mv_max) {
-        open(WF2, "> ./output_reduce_multiple_vector_matrix.txt") or die;
+        open(WF2, "> ./output_matrixvector.txt") or die;
         for ($j = $min_proc; $j <= $max_proc; $j = $j*2) {
             $result = 250000.0;
             for ($k = 0; $k < 3; $k++) {
