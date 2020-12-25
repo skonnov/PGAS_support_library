960aa1223f09642810038732769b17e0a30d4e6c
Sergey Konnov <sergeyzorro007@gmail.com>
2020-12-25 23:40:18 +0300

    some reorganising of parallel priority queue

--- include/parallel_priority_queue.h
+++ include/parallel_priority_queue.h
@@ -1,22 +1,26 @@
 #ifndef __PARALLEL_PRIORITY_QUEUE__
 #define __PARALLEL_PRIORITY_QUEUE__
 
+#include <climits>
 #include "parallel_vector.h"
 #include "memory_manager.h"
 #include "common.h"
 
 class parallel_priority_queue {
     int worker_rank, worker_size;
-    parallel_vector maxes, pqueues;  //, sizes;
+    parallel_vector maxes, sizes, pqueues;
     int num_of_quantums_proc, quantum_size, num_of_elems_proc;
-    int global_index_l, cur_worker, size_pqueue;
+    int global_index_l;
+    int default_value;
 public:
-    parallel_priority_queue(int _num_of_quantums_proc, int _quantum_size=DEFAULT_QUANTUM_SIZE);
+    parallel_priority_queue(int _num_of_quantums_proc, int _quantum_size=DEFAULT_QUANTUM_SIZE, int _default_value = INT_MIN);
     void insert(int elem);
     int get_max(int rank);
-    int size() const;
+    void remove_max();
 private:
+    void remove_max_internal();
     void insert_internal(int elem);
+    void heapify(int index);
 };
 
 #endif
