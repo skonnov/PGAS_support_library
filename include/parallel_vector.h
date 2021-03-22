0c22dcaac435ef18d745d818ed736697c1224dea
Sergey Konnov <sergeyzorro007@gmail.com>
2021-03-22 23:29:11 +0300

    fixes for types in parallel_vector

--- include/parallel_vector.h
+++ include/parallel_vector.h
@@ -12,8 +12,8 @@ class parallel_vector {
     int size_vector;  // глобальный размер вектора
 public:
     parallel_vector(const int& size=DEFAULT_QUANTUM_SIZE, const int& quantum_size = DEFAULT_QUANTUM_SIZE);
-    int get_elem(const int& index) const;  // получить элемент по глобальному индексу
-    void set_elem(const int& index, const int& value);  // сохранить элемент по глобальному индексу
+    T get_elem(const int& index) const;  // получить элемент по глобальному индексу
+    void set_elem(const int& index, const T& value);  // сохранить элемент по глобальному индексу
     void set_lock(int quantum_index);  // заблокировать квант
     void unset_lock(int quantum_index);  // разблокировать квант
     int get_quantum(int index);  // по глобальному индексу получить номер кванта
@@ -24,8 +24,9 @@ public:
     void read(const std::string& path, int number_of_elements);
     void read(const std::string& path, int number_of_elements, int offset, int num_elem_proc);
     void print(const std::string& path) const;
-    void change_mode(int quantum_index, int mode);
-    void change_mode(int quantum_index_l, int quantum_index_r, int mode);
+    void change_mode(int quantum_index, mods mode);
+    void change_mode(int quantum_index_l, int quantum_index_r, mods mode);
+    MPI_Datatype get_MPI_datatype() const;
 };
 
 template<class T>
@@ -35,14 +36,14 @@ parallel_vector<T>::parallel_vector(const int& number_of_elems, const int& quant
 }
 
 template<class T>
-int parallel_vector<T>::get_elem(const int& index) const {
+T parallel_vector<T>::get_elem(const int& index) const {
     if(index < 0 || index >= size_vector)
         throw -1;
     return memory_manager::get_data<T>(key, index);
 }
 
 template<class T>
-void parallel_vector<T>::set_elem(const int& index, const int& value) {
+void parallel_vector<T>::set_elem(const int& index, const T& value) {
     if(index < 0 || index >= size_vector)
         throw -1;
     memory_manager::set_data<T>(key, index, value);
@@ -99,13 +100,18 @@ void parallel_vector<T>::print(const std::string& path) const {
 }
 
 template<class T>
-void parallel_vector<T>::change_mode(int quantum_index, int mode) {
+void parallel_vector<T>::change_mode(int quantum_index, mods mode) {
     memory_manager::change_mode(key, quantum_index, quantum_index+1, mode);
 }
 
 template<class T>
-void parallel_vector<T>::change_mode(int quantum_index_l, int quantum_index_r, int mode) {
+void parallel_vector<T>::change_mode(int quantum_index_l, int quantum_index_r, mods mode) {
     memory_manager::change_mode(key, quantum_index_l, quantum_index_r, mode);
+}
+
+template<class T>
+MPI_Datatype parallel_vector<T>::get_MPI_datatype() const {
+    return memory_manager::get_MPI_datatype(key);
 }
 
 #endif // __PARALLEL_VECTOR_H__
