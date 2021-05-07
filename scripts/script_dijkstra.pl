26750a62d8e13f0212b36c0c211762f920c96178
Sergey Konnov <sergeyzorro007@gmail.com>
2021-05-07 22:54:46 +0300

    update script_dijkstra due to changes with args for dijkstra sample

--- scripts/script_dijkstra.pl
+++ scripts/script_dijkstra.pl
@@ -13,15 +13,14 @@ $path_dijkstra = ">> ./output_dijkstra_".$datetime.".txt";
 open(WF, $path_dijkstra) or die;
 
 for ($i = $it_min; $i < $it_max; $i += $it_step) {
-    $n = $i;
-    $m = $n * ($n-1) / 2;
+    $v = $i;
     for ($j = $min_proc; $j <= $max_proc; $j = $j*2) {
         $result = 250000.0;
         for ($k = 0; $k < 3; $k++) {
-            $tmp = `mpiexec -n $j ../build/Debug/dijkstra $n $m 150 0`;
+            $tmp = `mpiexec -n $j ../build/Debug/dijkstra -v $v -max 150`;
             $result = min($result + 0.0, $tmp+0.0);
         }
-        print "dijkstra algorithm for n = $n, m = $m, $j procs is done\n";
+        print "dijkstra algorithm for v = $v, $j procs is done\n";
         print WF $result, " ";
     }
     print WF "\n";
