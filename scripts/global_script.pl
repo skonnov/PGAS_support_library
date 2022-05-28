1b7d26b4dfe1a7bd65cac462958fea18a177dc44
Sergey Konnov <sergeyzorro007@gmail.com>
2022-05-29 00:35:20 +0300

    separate global_script on build and run parts

--- scripts/global_script.pl
+++ scripts/global_script.pl
@@ -1,26 +1,6 @@
 use List::Util qw[min max];
 use POSIX;
 
-use Cwd qw(abs_path);
-my $path = abs_path($0);
-
-print "Build library...\n";
-
-chdir('..') or die "$!";
-if (-d "build") {
-    if ($^O eq "linux") {
-        `rm -r build`;
-    } elsif ($^O eq "MSWin32") {
-        `rmdir build /s /q`;
-    }
-}
-`mkdir build`;
-chdir('build') or die "$!";
-`cmake ..`;
-`cmake --build . --config release`;
-chdir('../scripts') or die "$!";
-
-print "Library has been built successfully!\n";
 print "Run samples...\n";
 
 $it_sov = 5000000;
