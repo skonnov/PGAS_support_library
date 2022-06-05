60039bd72d8c5a635cc89c47fa65da6666a45ab4
Sergey Konnov <sergeyzorro007@gmail.com>
2022-06-06 01:20:37 +0300

    add print_to_file function and plots

--- scripts/parse_cache_miss_cnt_statistic_matrixmult.py
+++ scripts/parse_cache_miss_cnt_statistic_matrixmult.py
@@ -1,5 +1,8 @@
-from functools import cache
 import glob, sys
+import seaborn as sns
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 
 def parse_output_file(filename):
     f = open(filename)
@@ -41,11 +44,9 @@ def parse_cache_miss_cnt_statistics(info, filename, number_of_repeats):
         results_per_vector = []
         for i in range(number_of_repeats):
             for j in range(number_of_vectors):
-                print(info_index, info_proc_index, i, j)
                 if flag:
                     break
 
-                print(info[info_index]['processes_info'][info_proc_index]['minindex'])
                 if i != info[info_index]['processes_info'][info_proc_index]['minindex']:
                     for k in range(6):
                         line = f.readline()
@@ -53,8 +54,6 @@ def parse_cache_miss_cnt_statistics(info, filename, number_of_repeats):
                             flag = True
                             break
                     continue
-                
-                print("?")
 
                 line = f.readline()
                 if not line:
@@ -67,7 +66,7 @@ def parse_cache_miss_cnt_statistics(info, filename, number_of_repeats):
 
                 number_of_elements_in_vector = int(line[3][0:-1])
                 # number_of_processes = int(line[5][0:-1])
-                print(info[info_index]['number_of_elements'], number_of_elements_in_vector, info[info_index]['processes_info'][info_proc_index]['processes_count'])
+                # print(info[info_index]['number_of_elements'], number_of_elements_in_vector, info[info_index]['processes_info'][info_proc_index]['processes_count'])
                 assert(info[info_index]['number_of_elements'] ** 2 == number_of_elements_in_vector)
                 key = int(line[7][0:-1])
 
@@ -100,20 +99,66 @@ def parse_cache_miss_cnt_statistics(info, filename, number_of_repeats):
                 flag = True
                 break
     f.close()
-    
+
     return results
 
 def print_results_to_file(results_cache_miss_cnt, output_file):
     f = open(output_file, "w")
-    f.write("cache_size number_of_elements processes_count time cache_miss_cnt cache_evictions_cnt\n")
+    # f.write("cache_size number_of_elements processes_count time cache_miss_cnt cache_evictions_cnt\n")
+
+    cache_size = -1
+    number_of_elements = -1
+
+    f.write("time\n")
+    f.write("number_of_elements 2 5 10 17\n")
+
     for line in results_cache_miss_cnt:
+        if line['cache_size'] != cache_size:
+            cache_size = line['cache_size']
+            f.write("\n\ncache_size: " + str(line['cache_size']) + "\n")
+        if line['number_of_elements'] != number_of_elements:
+            number_of_elements = line['number_of_elements']
+            f.write('\n' + str(line['number_of_elements']) + " ")
+        f.write(str(line['time']) + " ")
+
+    f.write('\n~~~~~~~~~~\n')
+
+    cache_size = -1
+    number_of_elements = -1
+
+    f.write("cache_misses\n")
+    f.write("number_of_elements 2 5 10 17\n")
+
+    for line in results_cache_miss_cnt:
+        if line['cache_size'] != cache_size:
+            cache_size = line['cache_size']
+            f.write("\n\ncache_size: " + str(line['cache_size']) + "\n\n")
+        if line['number_of_elements'] != number_of_elements:
+            number_of_elements = line['number_of_elements']
+            f.write('\n' + str(line['number_of_elements']) + " ")
         total_total_cache_miss_cnt = sum([line['results_per_vector'][i]['total_cache_miss_cnt']
                                             for i in range(len(line['results_per_vector']))])
-        total_total_evictions_miss_cnt = sum([line['results_per_vector'][i]['total_cache_evictions_cnt']
+        f.write(str(total_total_cache_miss_cnt) + " ")
+
+    f.write('\n~~~~~~~~~~\n')
+
+    cache_size = -1
+    number_of_elements = -1
+
+    f.write("cache_evictions\n")
+    f.write("number_of_elements 2 5 10 17\n")
+
+    for line in results_cache_miss_cnt:
+        if line['cache_size'] != cache_size:
+            cache_size = line['cache_size']
+            f.write("\n\ncache_size: " + str(line['cache_size']) + "\n\n")
+        if line['number_of_elements'] != number_of_elements:
+            number_of_elements = line['number_of_elements']
+            f.write('\n' + str(line['number_of_elements']) + " ")
+        total_total_evictions_miss_cnt = sum([line['results_per_vector'][i]['total_cache_miss_cnt']
                                             for i in range(len(line['results_per_vector']))])
-        f.write(str(line['cache_size']) + " " + str(line['number_of_elements']) + " " +
-                str(line['processes_count']) + " " + str(line['time']) + " " +
-                str(total_total_cache_miss_cnt) + " " + str(total_total_evictions_miss_cnt) + "\n")
+        f.write(str(total_total_evictions_miss_cnt) + " ")
+
     f.close()
 
 if __name__ == "__main__":
@@ -126,7 +171,7 @@ if __name__ == "__main__":
     print("took file", file)
 
     results = parse_output_file(file)
-    print(results)
+    # print(results)
 
     file_cache_miss_cnt = "cache_miss_cnt.txt"
 
@@ -134,7 +179,69 @@ if __name__ == "__main__":
 
     results_cache_miss_cnt = parse_cache_miss_cnt_statistics(results, file_cache_miss_cnt, number_of_repeats)
 
-    print(results_cache_miss_cnt)
+    # print(results_cache_miss_cnt)
 
     output_file = "parsed_cache_miss_cnt.txt"
     print_results_to_file(results_cache_miss_cnt, output_file)
+
+    times = {}
+    number_of_elements = []
+    for line in results_cache_miss_cnt:
+        if line['cache_size'] not in times:
+            times[line['cache_size']] = {}
+        if line['processes_count'] not in times[line['cache_size']]:
+            times[line['cache_size']][line['processes_count']] = []
+
+        # change it?
+        if line['number_of_elements'] not in number_of_elements:
+            number_of_elements.append(line['number_of_elements'])
+
+        times[line['cache_size']][line['processes_count']].append(float(line['time']))
+
+    cache_evictions = {}
+    for line in results_cache_miss_cnt:
+        if line['cache_size'] not in cache_evictions:
+            cache_evictions[line['cache_size']] = {}
+        if line['processes_count'] not in cache_evictions[line['cache_size']]:
+            cache_evictions[line['cache_size']][line['processes_count']] = []
+        total_total_evictions_miss_cnt = sum([line['results_per_vector'][i]['total_cache_evictions_cnt']
+                                    for i in range(len(line['results_per_vector']))])
+        cache_evictions[line['cache_size']][line['processes_count']].append(float(total_total_evictions_miss_cnt))
+
+    accelerations = times.copy()
+    for cache_size in times:
+        accelerations[cache_size] = times[cache_size].copy()
+        for processes_count in times[cache_size]:
+            accelerations[cache_size][processes_count] = times[cache_size][processes_count].copy()
+            for i in range(len(times[cache_size][processes_count])):
+                accelerations[cache_size][processes_count][i] = times[cache_size][2][i] / times[cache_size][processes_count][i]
+
+    data_times = {cache_size:pd.DataFrame(accelerations[cache_size], index=number_of_elements) for cache_size in times}
+
+    data_cache_evictions = {cache_size:pd.DataFrame(cache_evictions[cache_size], index=number_of_elements) for cache_size in cache_evictions}
+
+
+    fig, axes = plt.subplots(len(times), 2, figsize=(12, 12))
+    fig.tight_layout(pad=5.0)
+    for i, cache_size in enumerate(data_times):
+        acceleration = sns.heatmap(ax=axes[i, 0], data=data_times[cache_size], cmap='coolwarm', vmin = 0., vmax = 7.0)
+        acceleration.set_title("размер кеша = " + str(cache_size), fontsize=14)
+        acceleration.set_xlabel("число процессов",fontsize=14)
+        acceleration.set_ylabel("число элементов",fontsize=14)
+
+        cache_evictions = sns.heatmap(ax=axes[i, 1], data=data_cache_evictions[cache_size], cmap='coolwarm')
+        cache_evictions.set_title("размер кеша = " + str(cache_size), fontsize=14)
+        cache_evictions.set_xlabel("число процессов",fontsize=14)
+        cache_evictions.set_ylabel("число элементов",fontsize=14)
+
+    fig, axes = plt.subplots(len(times) // 2, 2, figsize=(15, 10))
+    fig.tight_layout(pad=5.0)
+    for i, cache_size in enumerate(data_times):
+        lines = data_times[cache_size].transpose().plot.line(ax=axes[i // 2, i % 2])
+        lines.set_title("размер кеша = " + str(cache_size), fontsize=14)
+        lines.set_xticks([2, 5, 10, 17], fontsize=10)
+        lines.set_yticks(np.arange(0, 7, step=0.5),fontsize=10)
+        lines.set_xlabel("число процессов",fontsize=14)
+        lines.set_ylabel("ускорение",fontsize=14)
+
+    plt.show()
