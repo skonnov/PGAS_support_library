0eb857f1ef2fb814d8c2325a9783bcbae5c06ba3
Sergey Konnov <sergeyzorro007@gmail.com>
2022-11-09 00:34:15 +0300

    update schedule class

--- include/schedule.h
+++ include/schedule.h
@@ -5,15 +5,32 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include "common.h"
+
+enum schedule_enum {
+    SCHEDULE_CHANGE_MODE, SCHEDULE_GET_QUANTUM
+};
 
 struct schedule_line
 {
-
+    schedule_enum mode;
+    union {
+        int l;
+        int quantum_number;
+    };
+    union {
+        int r;
+        int number_of_process;
+    };
 };
 
 class schedule {
+    std::unordered_map<int, std::vector<schedule_line>*> schedule_structure;
 public:
-    void read_from_file(std::string path);
-
+    StatusCode read_from_file(std::string path);
+    void optimize();
+    std::unordered_map<int, std::vector<schedule_line>*> get() const;
+    ~schedule();
 };
 #endif
