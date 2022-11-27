da73e08c886826809a2825aaa7b9d698384e9f2c
Sergey Konnov <sergeyzorro007@gmail.com>
2022-11-27 23:50:44 +0300

    update statistic sample

--- src/schedule.cpp
+++ src/schedule.cpp
@@ -41,7 +41,7 @@ StatusCode schedule::read_from_file_schedule(std::string path) {
             schedule_structure[key].push_back({schedule_enum::SCHEDULE_GET_QUANTUM, quantum_number, process_number});
         }
     }
-
+    in.close();
     return STATUS_OK;
 }
 
@@ -65,10 +65,9 @@ StatusCode schedule::read_from_file_quantums_access_cnt(std::string path) {
     if (!in) {
         return STATUS_ERR_FILE_OPEN;
     }
-
     std::string line;
     std::getline(in, line); // skip first line
-    while(std::getline(in, line)) {
+    while (std::getline(in, line)) {
         std::vector<int> parsed_line = parse_string_line_quantums_access_cnt(line);
         int key = parsed_line[0], quantum_index = parsed_line[1], cnt = parsed_line[2], mode = parsed_line[3];
         quantum_cnts[key]; // create structure on key, if it didn't exist yet;
@@ -78,7 +77,7 @@ StatusCode schedule::read_from_file_quantums_access_cnt(std::string path) {
         quantum_cnts[key][quantum_index].push_back(cnt);
         modes[key][quantum_index].push_back(cnt);
     }
-
+    in.close();
     return STATUS_OK;
 }
 
