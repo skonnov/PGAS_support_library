ca6365f8ee21e41c800819d45acf364da8300b1e
Sergey Konnov <sergeyzorro007@gmail.com>
2022-12-05 23:33:39 +0300

    move cache_list class to separate file

---
+++ include/cache_list.h
@@ -0,0 +1,18 @@
+#include <mpi.h>
+#include "common.h"
+
+struct cache_node {
+    int value;
+    cache_node* next, *prev;
+};
+
+class cache_list {
+    cache_node* begin = nullptr, *end = nullptr;
+    int size = 0;
+public:
+    void push_back(cache_node* node);
+    cache_node* pop_front();
+    cache_node* pop_back();
+    void delete_node(cache_node* node);
+    bool empty();
+};
--- include/memory_cache.h
+++ include/memory_cache.h
@@ -8,22 +8,8 @@
 #include <iostream>
 #include <fstream>
 #include "common.h"
+#include "cache_list.h"
 
-struct cache_node {
-    int value;
-    cache_node* next, *prev;
-};
-
-class cache_list {
-    cache_node* begin = nullptr, *end = nullptr;
-    int size = 0;
-public:
-    void push_back(cache_node* node);
-    cache_node* pop_front();
-    cache_node* pop_back();
-    void delete_node(cache_node* node);
-    bool empty();
-};
 
 class memory_cache {
 public:
